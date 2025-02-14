0cb94e18e7c1de7a56df0e8406d7de8bc7f88175
Anand <akkit1648@gmail.com>
2025-02-14 15:27:31 +0530

    Feat: Refactor code for better readability and performance

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,21 +1,30 @@
+# frozen_string_literal: true
+
 require 'byebug'
 
 class StringCalculator
-	def self.add(numbers)
-		return 0 if numbers.empty?
-		
-		if numbers.start_with?("//")
-			delimiter, numbers = numbers.split("\n", 2)
-			delimiter = delimiter[2..]
-		else
-			delimiter = /,|\n/
-		end
+  def self.add(numbers)
+    return 0 if numbers.empty?
 
-		nums = numbers.split(delimiter).map(&:to_i)
-		negatives = nums.select(&:negative?)
-		raise "negative numbers not allowed #{negatives.join(',')}" unless negatives.empty?
+    delimiter, numbers = extract_delimiter_and_numbers(numbers)
+    nums = parse_numbers(numbers, delimiter)
 
-  	nums.sum
-	end
+    validate_no_negatives(nums)
+    nums.sum
+  end
+
+  def self.extract_delimiter_and_numbers(numbers)
+    return [numbers[2..].split("\n", 2).first, numbers.split("\n", 2).last] if numbers.start_with?('//')
+
+    [/[,\n]/, numbers]
+  end
+
+  def self.parse_numbers(numbers, delimiter)
+    numbers.split(delimiter).map(&:to_i)
+  end
+
+  def self.validate_no_negatives(nums)
+    negatives = nums.select(&:negative?)
+    raise "negative numbers not allowed #{negatives.join(',')}" if negatives.any?
+  end
 end
-  
