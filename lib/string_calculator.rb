293e335bbcc1c32573e7cb0825b4191e9b7408b1
Anand <akkit1648@gmail.com>
2025-02-14 13:50:35 +0530

    Feat: Implemented support for custom delimiters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,8 +3,15 @@ require 'byebug'
 class StringCalculator
 	def self.add(numbers)
 		return 0 if numbers.empty?
+		
+		if numbers.start_with?("//")
+			delimiter, numbers = numbers.split("\n", 2)
+			delimiter = delimiter[2..]
+		else
+			delimiter = /,|\n/
+		end
 
-		numbers.split(/,|\n/).map(&:to_i).sum
+		numbers.split(delimiter).map(&:to_i).sum
 	end
 end
   
